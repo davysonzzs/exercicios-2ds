b69abcd8f800c4897e1fe91f7993ee70b4a57fe9
davyson <davyssonlevi2.0@gmail.com>
2026-08-05 19:25:26 -0300

    exdercicios 1 e 2 feitos

---
+++ 3-bimestre/orientacao-a-objetos/aula02-classes-constructor-e-this/index.js
@@ -0,0 +1,11 @@
+class Funcionarios{
+    constructor(nome, cargo, salario){
+        this.nome = nome
+        this.cargo = cargo
+        this.salario = salario
+    }
+}
+
+const funcionarioDavyson = new Funcionarios("João", "Desenvolvedor", 5000)
+
+module.exports = funcionarioDavyson
