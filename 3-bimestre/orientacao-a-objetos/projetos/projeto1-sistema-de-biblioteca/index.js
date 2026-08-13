6490746db1ec7b1bb7a23061ccd8de528b06fe40
davyson <davyssonlevi2.0@gmail.com>
2026-08-13 11:49:16 -0300

    projeto-1-poo-feito

---
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto1-sistema-de-biblioteca/index.js
@@ -0,0 +1,32 @@
+class Livro{
+    constructor(titulo, autor, ano){
+        this.titulo = titulo
+        this.autor = autor
+        this.ano = ano
+        this.disponivel = true
+    }
+
+    emprestar(){
+        if(this.disponivel){
+            this.disponivel = false
+        }else{
+            return
+        }
+    }
+    devolver(){
+        if(!this.disponivel){
+            this.disponivel = true
+        } else{
+            return
+        }
+    }
+    estaDisponivel(){
+        if(this.disponivel){
+            return true
+        } else{
+            return false
+        }
+    }
+}
+
+module.exports = Livro
